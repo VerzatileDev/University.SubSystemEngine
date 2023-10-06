3a9ea0fa2689e380b63cde566b3dd627ef1372d4
VerzatileDev <lattb@coventry.ac.uk>
2023-10-06 21:34:31 +0300

    [ADD] / [FIX] Libraries/DLLs
    [ADD]
    - Core libraries were added such as box2D for physics and sfml for input handling or graphics.
    
    -Core Classes Added.
    
    [Fix]
    - DLL are now copied to debug folder correctly.
    - The search path for them is also in relation fixed to consider it.

--- SubSystemEngine/Application.cpp
+++ SubSystemEngine/Application.cpp
@@ -1,6 +1,26 @@
-
+#include "Box2D/Box2D.h"
+#include "SFML/Graphics.hpp"
 
 int main(int args, char* argv[])
 {
-	return 0;
+    // Create a window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(); // Clear the window
+
+        // Draw your content here
+
+        window.display(); // Display the content
+    }
+
+    return 0;
 }
